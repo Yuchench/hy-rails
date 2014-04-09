d2ac04df545b6ef3affdbd0d21f7783d8eefe127
Boska <boska@roamandwander.com>
2014-04-09 18:54:45 +0800

    Upload friend images via Paperclip

--- app/models/firework.rb
+++ app/models/firework.rb
@@ -2,7 +2,7 @@ class Firework < ActiveRecord::Base
 	has_attached_file :image, styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x300>'
+    medium: '600x600>'
   }
 
   # Validate the attached image is image/jpg, image/png, etc
