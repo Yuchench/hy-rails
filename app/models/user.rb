37dde0480aa93f135ab7e863d515a1ed0458450e
Boska <boska@roamandwander.com>
2014-04-17 17:37:54 +0800

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   if Rails.env.production?
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :rememberable, :trackable, :validatable
   else
     devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
   end
