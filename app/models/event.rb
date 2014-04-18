907a215e165ae19488e73e65dd872e45fae415b3
Boska <boska@roamandwander.com>
2014-04-18 10:35:20 +0800

    show only available event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+  scope :afterToday, where("event_date >= ?", Date.today).order('event_date')
   validates :url, :format => URI::regexp(%w(http https))
   validates :name, presence: true
   validates :event_date, presence: true
