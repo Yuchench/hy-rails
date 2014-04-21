3e1025d4ec86ab6a3cec5b2b740fe32cd2beed6e
Boska <boska@roamandwander.com>
2014-04-21 18:48:43 +0800

    picture and tube

--- app/helpers/fireworks_helper.rb
+++ app/helpers/fireworks_helper.rb
@@ -65,6 +65,16 @@ module FireworksHelper
       youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
       youtube_id = $5
     end
-    %Q{<img src="http://img.youtube.com/vi/#{ youtube_id }/0.jpg" class="img-thumbnail" width='100'>}.html_safe
+    %Q{<img src="http://img.youtube.com/vi/#{ youtube_id }/0.jpg">}.html_safe
+  end
+  def youtube_thumbnail_small(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+    %Q{<img src="http://img.youtube.com/vi/#{ youtube_id }/1.jpg">}.html_safe
   end
 end
