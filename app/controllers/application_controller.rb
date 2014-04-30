b9192be559bd64cc6a47855486acf01eb0d5dbdf
Boska <boska@roamandwander.com>
2014-04-30 16:27:18 +0800

    use basic auth to lock the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  http_basic_authenticate_with name: "hongyii@ms18.hinet.net", password: "mingyimingyi"
   protect_from_forgery with: :exception
 
   layout :layout_by_resource
