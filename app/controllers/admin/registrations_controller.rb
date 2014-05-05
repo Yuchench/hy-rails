bb41f7fe6455a169cf5ac8b644a35e71b02b6421
Boska <boska@roamandwander.com>
2014-05-05 18:39:35 +0800

    layout

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,6 +1,4 @@
 class Admin::RegistrationsController < Devise::RegistrationsController
-  layout 'admin'
-
   def new
     flash[:info] = 'Registrations are not open yet, but please check back soon'
     redirect_to root_path
